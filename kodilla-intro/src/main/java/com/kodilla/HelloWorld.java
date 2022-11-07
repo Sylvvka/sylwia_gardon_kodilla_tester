32ade9fcdb0ca3451aee4ac0af2581983fffb69a
Sylwia Gardon <sylwia.gardon@gmail.com>
2022-11-07 20:13:40 +0100

    Proby

---
+++ kodilla-intro/src/main/java/com/kodilla/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.kodilla;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Sylwia");
+        System.out.println("49");
+        System.out.println("G");
+    }
+}
